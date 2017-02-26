723645c952342c7981c77b201bd27adf16c8bb21
João Ferreira <joao12ferreira@gmail.com>
2017-02-26 23:39:46 +0000

    Initial Commit
    Visual Studio Project and WSP solution

---
+++ Visual Studio Project/SharePointGoogleAnalytics/Features/GoogleAnalytics feature/GoogleAnalytics feature.feature
@@ -0,0 +1,7 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="32e42e8c-7967-4008-981f-6e09e0188d3b" description="This feature adds google analytics to all sites in this site collection without modifying the master page." featureId="32e42e8c-7967-4008-981f-6e09e0188d3b" imageUrl="GenericFeature.gif?spanalytics.png" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="SharePoint Google Analytics" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="28b16657-32fa-404f-88b6-44886876503f" />
+    <projectItemReference itemId="a7384aa4-6a71-42de-84c3-63ea79a4bbde" />
+  </projectItems>
+</feature>
---
+++ Visual Studio Project/SharePointGoogleAnalytics/Style Library/GoogleAnalytics/js/ga.js
@@ -0,0 +1,39 @@
+﻿(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-92611503-1', 'auto');
+ga('send', 'pageview');
+
+/*The code bellow is not part of the google analytics and can be removed if needed*/
+/*This code adds the custom icon to site collection feaures*/
+document.addEventListener("DOMContentLoaded", function () {
+    var pathGoogleAnalytics = "";
+    var scripts = document.getElementsByTagName("script");
+
+    //gets the location of the breadcrumb script to inject the css
+    if (scripts && scripts.length > 0) {
+        for (var i in scripts) {
+            if (scripts[i].src && scripts[i].src.match(/ga\.js$/)) {
+                pathGoogleAnalytics = scripts[i].src.replace(/(.*)ga\.js$/, "$1");
+                pathGoogleAnalytics = pathGoogleAnalytics.replace("/js/", "/");
+                break;
+            }
+        }
+    };
+
+    //Feature Images
+    findImagesByRegexp();
+    function findImagesByRegexp() {
+        var images = Array.prototype.slice.call((document.getElementById('DeltaPlaceHolderMain') || document).getElementsByTagName('img'));
+        var length = images.length;
+        for (var i = 0; i < length; ++i) {
+            if (images[i].src.indexOf('spanalytics.png') != -1) {
+                images[i].src = pathGoogleAnalytics + '/images/spanalytics.png';
+            }
+        }
+    }
+});
